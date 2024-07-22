0d4a2075a6bbc1922b48e96916263a8775f42318
Anderson Almada <andersonalmadagomes@gmail.com>
2024-07-22 12:06:04 -0300

    ok

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Habilita o CORS para todas as rotas
 
 def parse_params(param1, param2):
     try:
